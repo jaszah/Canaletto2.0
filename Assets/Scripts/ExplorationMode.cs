1af516463036385ca8e52c70b01fbe74309acac8
Hekutoki <igamm2@gmail.com>
2020-09-10 18:16:54 +0200

    update kodu ExplorationMode

--- Assets/Scripts/ExplorationMode.cs
+++ Assets/Scripts/ExplorationMode.cs
@@ -1,20 +1,30 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class ExplorationMode : MonoBehaviour
 {
 	public static int objectsCount = 19;
+	public GameObject blend;
+	public GameObject masks;
+	public GameObject button;
+
+	private static string objectName;
 
 	private void Start()
 	{
 		objectsCount = GameObject.FindGameObjectsWithTag("ObjecToFind").Length;
-		Debug.Log(objectsCount);
 	}
 
 	private void OnMouseDown()
 	{
-		//this.gameObject.SetActive(false);
-		this.gameObject.GetComponent<SpriteMask>().frontSortingOrder = 1;
+		this.gameObject.GetComponent<SpriteMask>().isCustomRangeActive = false;
+		blend.SetActive(true);
+
+		objectName = this.gameObject.name;
+
+		PrintText();
+
 		objectsCount--;
 
 		if (objectsCount == 0)
@@ -27,5 +37,24 @@ public class ExplorationMode : MonoBehaviour
 	private void NextSceneTransition()
 	{
 		SceneManager.LoadScene(1);
+	}
+
+	private void PrintText()
+	{
+		//tutaj wstawić tekst
+		button.SetActive(true);
+	}
+
+	public static void SetVisible(string name)
+	{
+		GameObject.Find(name).gameObject.GetComponent<SpriteMask>().isCustomRangeActive = true;
+	}
+
+	public void OkButton()
+	{
+		masks.SetActive(true);
+		blend.SetActive(false);
+
+		GameObject.Find(objectName).GetComponent<SpriteMask>().isCustomRangeActive = true;
 	}
 }
